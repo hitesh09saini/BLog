bd2e70669c45c86d29797e4817f7d3a9f79d3e0b
Hitesh Saini <hitesh9694saini@gmail.com>
2023-10-30 12:05:27 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 
-
-
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   return (
     <>
      
---
+++ src/appwrite/auth.js
@@ -0,0 +1,15 @@
+import conf from '../conf/conf'
+
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client  =  new Client();
+   
+
+}
+
+const authService = new AuthService();
+
+
+export default AuthService;
