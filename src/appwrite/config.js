dc8be2cff33652adae3f53a45a1172572a57b25a
Hitesh Saini <hitesh9694saini@gmail.com>
2023-11-04 15:10:01 +0530

    up

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -2,37 +2,37 @@ import conf from '../conf/conf'
 
 import { Client, ID, Databases, Storage, Query } from 'appwrite'
 
-export class Service{
+export class Service {
     client = new Client();
     databases;
     bucket;
 
-    constructor(){
+    constructor() {
         this.client.setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+            .setProject(conf.appwriteProjectId);
 
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title, slug, content, featuredImg, status, userId}){
+    async createPost({ title, slug, content, featuredImg, status, userId }) {
         try {
 
-         return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, 
-         {
-            title, 
-            content, 
-            featuredImg, 
-            status, 
-            userId
-         })
-            
+            return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug,
+                {
+                    title,
+                    content,
+                    featuredImg,
+                    status,
+                    userId
+                })
+
         } catch (error) {
             throw error;
         }
     }
 
-    async updatePost(slug, {title, content, featuredImg, status}){
+    async updatePost(slug, { title, content, featuredImg, status }) {
         try {
             return await this.databases.updateDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
                 title,
@@ -45,34 +45,76 @@ export class Service{
         }
     }
 
-    async deletePost(slug){
+    async deletePost(slug) {
         try {
-             await this.databases.deleteDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
+            await this.databases.deleteDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
 
-             return true;
+            return true;
         } catch (error) {
             throw error;
         }
     }
 
-    async getPost(slug){
+    async getPost(slug) {
         try {
 
             return await this.databases.deleteDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug);
-            
+
         } catch (error) {
             throw error;
         }
     }
 
-    async getPosts(queries){
+    async getPosts(queries = [Query.equal('status', "active")]) {
         try {
-            return 
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
         } catch (error) {
             throw error;
+            return false
+        }
+
+    }
+
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            throw error;
+            return false
         }
     }
 
+    async deleteFile(fileId) {
+        try {
+           await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId
+           )
+
+           return true;
+
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service();
